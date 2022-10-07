f23d2e646948939eac7901773556c505f6f1fb00
Edwin1403 <cyedwin4@gmail.com>
2022-10-07 15:12:23 +0530

    git actions

--- Route/user.py
+++ Route/user.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter,Depends,status,HTTPException
 from sqlalchemy.orm import Session
-from ..import model,schema, passcode , auth
-from ..database import get_db
+import model,schema, passcode , auth
+from database import get_db
 from fastapi.security import OAuth2PasswordRequestForm
 
 meth =APIRouter (
